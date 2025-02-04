c75645d4a4eac895e30d01909755aeb538132320
tailorraj <tailorraj111@gmail.com>
2025-02-04 12:47:56 +0530

    boutique address changes

--- crm_application_plugin/crm_application_plugin/doctype/boutique/boutique.js
+++ crm_application_plugin/crm_application_plugin/doctype/boutique/boutique.js
@@ -5,4 +5,22 @@ frappe.ui.form.on('Boutique', {
 	// refresh: function(frm) {
 
 	// }
+	boutique_location:function(frm){
+		if(frm.doc.boutique_location){
+			return frm.call({
+			method: "frappe.contacts.doctype.address.address.get_address_display",
+			args: {
+				"address_dict": frm.doc.boutique_location
+			},
+			callback: function(r) {
+				if(r.message)
+					frm.set_value("boutique_location_display", r.message);
+				}
+			});
+			  
+		}
+		else{
+			frm.set_value("boutique_location_display", "");
+		}
+	}
 });
